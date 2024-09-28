0bd5fe48eaf533de323784c5fc225e459b3645ef
Albert <153892855+Albrt78@users.noreply.github.com>
2024-09-28 13:46:11 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const button = document.getElementById("btn");
 const alert = document.getElementById("alert");
 const input = document.getElementById("email");
-const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,3}$/;
 
 function emailValidation() {
     input.value.match(emailRegex)
